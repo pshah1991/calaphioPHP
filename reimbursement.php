746886b378e546641dd0bcca8c20015a1eba37b9
pshah1991 <pshah1991@gmail.com>
2015-03-02 01:57:01 -0800

    Update language
    Updated the reimbursement rules to match C slide. Make the language more clear.

--- reimbursement.php
+++ reimbursement.php
@@ -5,22 +5,24 @@ require("include/Template.class.php");
 Template::print_head(array());
 Template::print_body_header('Calendar', 'REIMBURSE');
 ?>
-<h2><b>How Reimbursements Work</b></h2><br/>
-<p>You need the receipt and write down these information for reimbursements  <br/>
-and give it to either the Finance VP or Reimbursement Chair: <br/><br/>
-Name <br/>
-SID <br/>
-E-Mail <br/>
-Address <br/>
-Phone Number <br/>
-Reason for Reimbursement<br/><br/>
-Don't forget that you can get half your public transportation fee reimbursed too, <br />
-such as BART tickets if you get the receipt by paying with credit card. <br />
-However, if you cross any toll bridge, ask for a receipt and you'll get reimbursed full if you are driving. <br />
-*Note: these only pertain to service events <br />
-<h2><b>Driver Reimbursement Form</b></h2><br/>
-<p><a href="/documents/TransportReimbForm.pdf"> TransportReimbForm.pdf </a></p><br/>		
-<p>Only for service events. Fill out all fields except "Bus fare" and "BART fare," attach receipts, and turn into reimbursement chair.</p>
+<h2><b>Reimbursements</b></h2><br/>
+<p> In order to get reimbursed for an expense, you need the receipt.<br>
+Write down the information below on the back or on an additional piece of paper<br/>
+and give it to either the Finance VP or one of the Reimbursement Chairs: <br/><br/>
+<b>Name: <br/>
+SID: <br/>
+E-Mail: <br/>
+Address: <br/>
+Phone Number: <br/>
+Reason for Reimbursement/Name of Service Project:<br/><br/>
+
+<h2><b>Service Project Reimbursements</h2></b>
+<li>You can get your public transportation or mileage fees reimbursed too! </li> <br>
+<li>Get reimbursed for BART tickets (pay by Credit Card), Bridge Toll and Parking if you get the receipt!</li><br />
+<li>Drivers: If you cross any toll bridge be sure to ask for a receipt and you'll get reimbursed full </li> <br />
+<li>Be sure to fill out the <a href"/documents/TransportReimbForm.pdf"> Transportation Reimbursement Form </a>' upon submission!<br>
+<li>Fill out all applicable fields, attach receipts, and turn into Finance VP or reimbursement chair.</li><br>
+
 <?php
 Template::print_body_footer();
 Template::print_disclaimer();
